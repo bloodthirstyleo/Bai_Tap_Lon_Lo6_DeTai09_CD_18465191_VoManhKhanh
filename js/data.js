f159ec091206b71d3ce89c813f4708fa2967f9d5
bloodthirstyleo <50875125+bloodthirstyleo@users.noreply.github.com>
2022-04-17 05:26:40 +0700

    Hoàn thành

---
+++ js/data.js
@@ -0,0 +1,115 @@
+var valueData = [
+    {
+        "id": "MD0001",
+        "price": "",
+        "title": "Thiết bị chuyển mạch Ruijie Switch RG-ES108GD (8-Port 100/1000MB RJ45 ) - Vỏ kim loại",
+        "image": "../img/item1.jpg",
+        "url": "/MD0001.html"
+    },
+    {
+        "id": "CW0001",
+        "price": "790.000 VND",
+        "title": "Card Wifi PC D-Link DWA 582",
+        "image": "../img/item16.jpg",
+        "url": "/CW0001.html"
+    },
+    {
+        "id": "WF0001",
+        "price": "910.000 VND",
+        "title": "Bộ Định Tuyến TP-Link Archer C6 V3.20 AC1200 Wireless MU-MIMO Gigabit Router",
+        "image": "../img/item3.jpg",
+        "url": "/WF0001.html"
+    },
+    {
+        "id": "WF0002",
+        "price": "490.000 VND",
+        "title": "Bộ Định Tuyến TP-Link Archer C6 V3.20 AC1200 Wireless MU-MIMO Gigabit Router",
+        "image": "../img/item4.jpg",
+        "url": "/WF0002.html"
+    },
+    {
+        "id": "WF0003",
+        "price": "1.290.000 VND",
+        "title": "Bộ Định Tuyến TP-Link Archer AX10 AX1500 Wi-Fi 6 Router",
+        "image": "../img/item5.jpg",
+        "url": "/WF0003.html"
+    },
+    {
+        "id": "WF0004",
+        "price": "335.000 VND",
+        "title": "Bộ Định Tuyến Asus RT-N12+ WiFi Router",
+        "image": "../img/item6.png",
+        "url": "/WF0004.html"
+    },
+    {
+        "id": "WF0005",
+        "price": "14.490.000 VND",
+        "title": "ASUS ROG Rapture GT-AX11000 (Gaming Router) Wifi AX11000 3 băng tần 10 Gigabit, AURA RGB",
+        "image": "../img/item7.png",
+        "url": "/WF0005.html"
+    },
+    {
+        "id": "CW0002",
+        "price": "95.000 VND",
+        "title": "TP-Link TG-3468",
+        "image": "../img/item8.jpg",
+        "url": "/CW0002.html"
+    },
+    {
+        "id": "WF0006",
+        "price": "2.850.000 VND",
+        "title": "Bộ phát wifi 6 Linksys E8450-AH MAX-STREAM AX3200 EASY MESH MU-MIMO",
+        "image": "../img/item9.jpeg",
+        "url": "/WF0006.html"
+    },
+    {
+        "id": "WF0007",
+        "price": "4.099.000 VND",
+        "title": "Bộ phát wifi ASUS XD4 (B-2-PK) Mesh wifi 6, Chuẩn AX1800 - màu đen",
+        "image": "../img/item10.jpg",
+        "url": "/WF0007.html"
+    },
+    {
+        "id": "CW0003",
+        "price": "790.000 VND",
+        "title": "Card Wifi PC D-Link DWA 582",
+        "image": "../img/item11.jpg",
+        "url": "/CW0003.html"
+    }
+    ,
+    {
+        "id": "WF0010",
+        "price": "590.000 VND",
+        "title": "Thiết bị mạng TPlink EAP110 Wireless-N 300Mbps Access Point",
+        "image": "../img/item12.jpg",
+        "url": "/WF0010.html"
+    },
+    {
+        "id": "DM0001",
+        "price": "490.000 VND",
+        "title": "Đầu mạng Cat 5 Dintek (100 đầu), chính hãng",
+        "image": "../img/item13.jpg",
+        "url": "/DM0001.html"
+    },
+    {
+        "id": "WF0008",
+        "price": "429.000 VND",
+        "title": "TP-Link TL-WR940N",
+        "image": "../img/item15.jpg",
+        "url": "/WF0008.html"
+    },
+    {
+        "id": "DM0002",
+        "price": "69.000 VND",
+        "title": "Cáp mạng Orico PUG-C6-30 CAT6 3m 26AWG",
+        "image": "../img/item14.jpg",
+        "url": "/DM0002.html"
+    },
+    {
+        "id": "WF0009",
+        "price": "1.890.000 VND",
+        "title": "Bộ định tuyến ASUS RT-AX55 - AX1800 Dual Band WiFi 6 Gaming Router",
+        "image": "../img/item2.jpg",
+        "url": "/WF0009.html"
+    }
+]
--- js/main.js
+++ js/main.js
@@ -1,78 +1,7 @@
 $(document).ready(function () {
     var checkLoginUser = 1;
     var checkLoginPassWord = 1;
-    var valueData = [
-        {
-            "id": "36403",
-            "price": "2699000",
-            "title": "Mainboard ASROCK B560M Pro4",
-            "image": "https://anphat.com.vn/media/product/75_36403_b560m_pro4_l1_.png",
-            "url": "/mainboard-asrock-b560m-pro4.html"
-        },
-        {
-            "id": "36639",
-            "price": "3599000",
-            "title": "Mainboard ASUS ROG STRIX B560-G GAMING WIFI",
-            "image": "https://anphat.com.vn/media/product/75_36639_h732__9_.png",
-            "url": "/mainboard-asus-rog-strix-b560-g-gaming-wifi_id36639.html"
-        },
-        {
-            "id": "36640",
-            "price": "4399000",
-            "title": "Mainboard ASUS ROG STRIX B560-I GAMING WIFI",
-            "image": "https://anphat.com.vn/media/product/75_36640_h732.png",
-            "url": "/mainboard-asus-rog-strix-b560-i-gaming_id36640.html"
-        },
-        {
-            "id": "36937",
-            "price": "1679000",
-            "title": "Mainboard ASROCK H510M-HVS ",
-            "image": "https://anphat.com.vn/media/product/75_36937_h510m_hvs_l1_.png",
-            "url": "/mainboard-asrock-h510m-hvs_id36937.html"
-        },
-        {
-            "id": "36938",
-            "price": "1699000",
-            "title": "Mainboard ASROCK H510M-HDV",
-            "image": "https://anphat.com.vn/media/product/75_36938_h510m_hdv_l1_.png",
-            "url": "/mainboard-asrock-h510m-hdv_id36938.html"
-        },
-        {
-            "id": "37021",
-            "price": "2640000",
-            "title": "Màn hình máy tính HP V19 18.5 inch 9TN41AA",
-            "image": "https://anphat.com.vn/media/product/75_37021_c06694167.png",
-            "url": "/man-hinh-may-tinh-hp-v19-18.5-inch-9tn41aa.html"
-        },
-        {
-            "id": "37309",
-            "price": "9249000",
-            "title": "Vỏ case Asus ROG STRIX HELIOS White GUNDAM LIMITED",
-            "image": "https://anphat.com.vn/media/product/75_37309_rog_strix_helios_gundam_ltd_01_t.jpg",
-            "url": "/vo-case-asus-rog-strix-helios-white-gundam-limited_id37309.html"
-        },
-        {
-            "id": "37742",
-            "price": "7590000",
-            "title": "Màn hình máy tính HP E24T G4 23.8 inch FHD IPS 9VH85AA - Cảm ứng",
-            "image": "https://anphat.com.vn/media/product/75_37742_hp_e24t_g4.jpg",
-            "url": "/man-hinh-may-tinh-hp-e24t-g4-23.8-inch-fhd-ips-9vh85aa-cam-ung.html"
-        },
-        {
-            "id": "38423",
-            "price": "5389000",
-            "title": "Màn hình máy tính HP M27f 2H0N1AA 27 inch FHD IPS 60Hz",
-            "image": "https://anphat.com.vn/media/product/75_38423_screenshot_3.png",
-            "url": "/man-hinh-may-tinh-hp-m27f-2h0n1aa-27-inch-fhd-ips-60hz.html"
-        },
-        {
-            "id": "38424",
-            "price": "5449000",
-            "title": "Màn hình máy tính HP M27fw 2H1B1AA 27 inch FHD IPS ",
-            "image": "https://anphat.com.vn/media/product/75_38424_screenshot_10.png",
-            "url": "/man-hinh-may-tinh-hp-m27fw-2h1b1aa-27-inch-fhd-ips.html"
-        }
-    ]
+    
      //Đây là hàm search
     $("#search-product").keyup(function () {
         console.log("ok");
